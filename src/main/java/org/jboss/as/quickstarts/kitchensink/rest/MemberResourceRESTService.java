6fd8dd59135e3e1c37a4575a31700d9eedeca590
akshay <akshay@ubuntu>
2013-03-24 20:20:32 +0100

    Commit from JBoss Tools

--- src/main/java/org/jboss/as/quickstarts/kitchensink/rest/MemberResourceRESTService.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/rest/MemberResourceRESTService.java
@@ -7,7 +7,6 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
@@ -42,13 +41,6 @@ public class MemberResourceRESTService {
       final List<Member> results = em.createQuery("select m from Member m order by m.name").getResultList();
       clientApi.createNewUser("123", "123", null);
       return results;
-   }
-
-   @GET
-   @Path("/{id:[0-9][0-9]*}")
-   @Produces("text/xml")
-   public Member lookupMemberById(@PathParam("id") long id) {
-      return em.find(Member.class, id);
    }
    
    @GET
